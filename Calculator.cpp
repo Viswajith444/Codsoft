ddf527b5331eea7809e9c00f733ae35a71b03085
Viswajith444 <viswajith108@gmail.com>
2024-03-10 01:16:02 +0530

    incomplete code

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,14 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <string>
+#include <stack>
 // #include <conio.h>
 
 
 template<typename T>
 class Calculator{
     private:
-    
+        std::string post_fix_expression;
 
     public:
+
+        Calculator(std::string input) : post_fix_expression(input){}
+
         T add( T a, T b){
             return a + b;
         }
@@ -31,6 +36,25 @@ class Calculator{
 
 };
 
+bool fn(char ch){ // please delete me and do hash table
+    switch (ch)
+        {
+            case '+':
+                return 1;
+                
+            case '-':
+                return 1;
+                
+            case '*':
+                return 1;
+                
+            case '/':
+                return 1;
+                
+            default:
+                return 0;
+        }
+}
 int main(){
 
     double a,b;
@@ -39,32 +63,49 @@ int main(){
 
         printf("\x1b[2J");
         printf("\x1b[H");
+        std::string input;
         // printf("\x1b[uANSISYSRC");
-        scanf("%lf %c %lf", &a, &ch, &b);
-        getchar();
-
+        // scanf("%lf %c %lf", &a, &ch, &b);
+        // getchar();
+        std::cin >>  input;
+        
+        std::stack<char> operand, op;
+        std::string post_fix_expression = ""; 
+        
+        for(char& ch: input){
+            if(isdigit(ch)){
+                operand.push(ch);
+            }
+            else if(fn(ch)){
+                post_fix_expression += operand.top();
+                operand.pop();
+                post_fix_expression += operand.top();
+                operand.pop();
+                post_fix_expression += ch;
+            }
+        }
         double ans;
         switch (ch)
         {
-        case '+':
-            ans = a + b;
-            break;
-        
-        case '-':
-            ans = a - b;
-            break;
+            case '+':
+                ans = a + b;
+                break;
+            
+            case '-':
+                ans = a - b;
+                break;
 
-        case '*':
-            ans = a * b;
-            break;
+            case '*':
+                ans = a * b;
+                break;
 
-        case '/':
-            ans = a / b;
-            break;
+            case '/':
+                ans = a / b;
+                break;
 
-        default:
-            std::cout  << "Error" << std::endl;
-            continue;
+            default:
+                std::cout  << "Error" << std::endl;
+                continue;
         }
 
         std::cout << "Result: " << ans << "\nPress any key to continue" << std::endl;
